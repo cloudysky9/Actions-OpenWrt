09b4a63747b53bd9fcbfc14d46ca8d1e38a8ba57
cloudysky9 <64485673+cloudysky9@users.noreply.github.com>
2021-01-17 18:26:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,15 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+#添加旁路由防火墙
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+#修改版本号
+#sed -i 's/OpenWrt/cloudysky AutoBuild $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settingssky
+echo '修改机器名称'
+sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
+# Timezone
+#sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
+#sed -i "/timezone='CST-8'/a set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
+# Mod zzz-default-settings
+sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/rosy'" package/lean/default-settings/files/zzz-default-settings
