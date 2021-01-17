e650c3c4d0caa892313d940b06c806db88facada
cloudysky9 <64485673+cloudysky9@users.noreply.github.com>
2021-01-17 18:25:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,18 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
+git clone https://github.com/cloudysky9/luci-theme-rosy.git package-temp/luci-theme-rosy
+git clone https://github.com/xiaorouji/openwrt-passwall.git package-temp/openwrt-passwall
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package-temp/luci-app-jd-dailybonus
+mv -f package-temp/lua-maxminddb package/lean/
+mv -f package-temp/luci-app-vssr package/lean/
+mv -f package-temp/luci-theme-rosy package/lean/
+mv -f package-temp/openwrt-passwall package/lean/
+mv -f package-temp/luci-app-jd-dailybonus package/lean/
+rm -rf package-temp
