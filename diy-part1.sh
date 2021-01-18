f88861d9969852718771b5f861194449e303a8c6
cloudysky9 <64485673+cloudysky9@users.noreply.github.com>
2021-01-19 00:09:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,14 +15,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
-git clone https://github.com/cloudysky9/luci-theme-rosy.git package-temp/luci-theme-rosy
-git clone https://github.com/xiaorouji/openwrt-passwall.git package-temp/openwrt-passwall
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package-temp/luci-app-jd-dailybonus
-mv -f package-temp/lua-maxminddb package/lean/
-mv -f package-temp/luci-app-vssr package/lean/
-mv -f package-temp/luci-theme-rosy package/lean/
-mv -f package-temp/openwrt-passwall package/lean/
-mv -f package-temp/luci-app-jd-dailybonus package/lean/
-rm -rf package-temp
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/cloudysky9/luci-theme-rosy.git package/luci-theme-rosy
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
